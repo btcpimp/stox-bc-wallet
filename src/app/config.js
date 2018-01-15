96f3c430f407d4f0d73cbfad8e425b6d377ffe6d
Danny <danny@stox.com>
2018-01-15 15:49:48 +0200

    Added config items

--- src/app/config.js
+++ src/app/config.js
@@ -4,4 +4,6 @@ module.exports = {
   PORT: process.env.PORT,
   WEB3_URL: process.env.WEB3_URL || 'http://localhost:8545/',
   DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:secret@localhost/stox2',
+  requiredConfirmations: 12,
+  maxBlocksRead: 0,
 }
