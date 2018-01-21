f34f5379ce0f6a0d0c12d999ff1ea4f7e5514885
Yair Oval <yairov@gmail.com>
2018-01-21 08:52:13 +0200

    add .dockerignore file

--- src/app/config.js
+++ src/app/config.js
@@ -3,7 +3,7 @@ const {configs: {mapEnv}} = require('@welldone-software/node-toolbelt')
 module.exports = mapEnv({
   port: 3001,
   web3Url: 'http://10.250.2.207:8545/',
-  databaseUrl: 'postgres://postgres:secret@db/stox2',
+  databaseUrl: 'postgres://postgres:secret@stox_postgress/stox2',
   requiredConfirmations: 12,
   maxBlocksRead: 10000,
 })
