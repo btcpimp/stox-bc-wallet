cef44d5109df5c25621fe7eba680f65123d4ec11
Yair Oval <yairov@gmail.com>
2018-01-23 09:40:02 +0200

    fix 'assignWallet' query

--- src/app/services/wallets.js
+++ src/app/services/wallets.js
@@ -1,10 +1,13 @@
 const {exceptions: {NotFoundError}} = require('@welldone-software/node-toolbelt')
+const Sequelize = require('sequelize')
 const db = require('app/db')
+
+const {Op} = Sequelize
 
 const createWallet = async () => db.wallet.create({address: new Date().getTime(), assignedAt: null})
 
 const assignWallet = async () => {
-  const wallet = await db.wallet.findOne({where: {assignedAt: null}})
+  const wallet = await db.wallets.findOne({where: {assignedAt: {[Op.eq]: null}}})
 
   if (!wallet) {
     throw new NotFoundError('no wallets to assign')
