80bf5b8dcb694eaf5cc540026c58a68335aa4756
Yair Oval <yairov@gmail.com>
2018-02-05 15:38:06 +0200

    update backend url

--- src/app/services/frontendApi.js
+++ src/app/services/frontendApi.js
@@ -1,10 +1,10 @@
 const axios = require('axios')
 const {exceptions: {UnexpectedError}} = require('@welldone-software/node-toolbelt')
-const {frontendBaseUrl} = require('../config')
+const {backendBaseUrl} = require('../config')
 
 const http = (() => {
   const ax = axios.create({
-    baseURL: frontendBaseUrl,
+    baseURL: `${backendBaseUrl}/api/v1`,
     responseType: 'json',
   })
   return ['post', 'get', 'delete', 'put'].reduce((caller, method) => {
