0d9398c7e38613acdc16050756bccec0fa8067ee
Yair Oval <yairov@gmail.com>
2018-02-07 09:51:58 +0200

    remove default config

--- src/app/config.js
+++ src/app/config.js
@@ -2,13 +2,13 @@ const {configs: {mapEnv}} = require('@welldone-software/node-toolbelt')
 
 module.exports = mapEnv({
   port: 3001,
-  web3Url: 'http://10.250.2.207:8545/',
-  databaseUrl: 'postgres://postgres:secret@bc_wallet_db/stox2',
+  web3Url: '',
+  databaseUrl: '',
   requiredConfirmations: 12,
   maxBlocksRead: 10000,
   network: '',
-  tokenTransferCron: '1 * * * * *', // cron syntax,
-  updateBalanceCron: '1 * * * * *', // cron syntax,
+  tokenTransferCron: '*/20 * * * * *', // cron syntax,
+  updateBalanceCron: '*/20 * * * * *', // cron syntax,
   maxWalletAssignRetires: 5,
-  backendBaseUrl: 'http://localhost:3002',
+  backendBaseUrl: '',
 })
