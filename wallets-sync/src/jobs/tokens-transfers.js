8b682e09e64107c62d8d26deaa09a5eb76de3640
Yair Oval <yairov@gmail.com>
2018-02-27 17:58:48 +0200

    refactoring wallets subsystem

--- wallets-sync/src/jobs/tokens-transfers.js
+++ wallets-sync/src/jobs/tokens-transfers.js
@@ -3,7 +3,7 @@ const tokensTransfers = require('common/src/services/tokensTransfers')
 const {network, tokenTransferCron, maxBlocksRead, requiredConfirmations} = require('../config')
 
 module.exports = {
-  cron: '*/5 * * * *',
+  cron: '*/5 * * * * *',
   job: async () => {
     const options = {network, tokenTransferCron, maxBlocksRead, requiredConfirmations}
     const transactions = await tokensTransfers.fetchTransactions(options)
