83539c299e8b3b5cafbe79220fab10ef24e67d38
Yair Oval <yairov@gmail.com>
2018-03-07 21:40:28 +0200

    fix api

--- wallets-api/src/api.js
+++ wallets-api/src/api.js
@@ -1,15 +1,33 @@
 const {port} = require('config')
-const {services: {db}} = require('stox-bc-wallet-common')
+const {services: {wallets, tokensBalances}} = require('stox-bc-wallet-common')
 
 module.exports = {
   port,
   version: 1,
   routes: (router, _) => {
-    router.get('/unassigned/count', _(() => db.wallets.getUnassignedWalletsCount()))
-    router.get('/balance', _(({query: {address}}) => db.tokensBalances.getBalance(address)))
-    router.post('/assign', _(({body: {address}}) => db.wallets.assignWallet(address)))
-    router.post('/create', _(({body: {address}}) => db.wallets.createWallet(address)))
-    router.post('/createWallets', _(({body: {addresses}}) => db.wallets.createWallets(addresses)))
-    router.get('/wallets/blockchainBalance', _(({query: {address}}) => db.wallets.getWalletBalanceInBlockchain(address)))
+    router.get(
+      '/unassigned/count',
+      _(() => wallets.getUnassignedWalletsCount())
+    )
+    router.get(
+      '/balance',
+      _(({query: {address}}) => tokensBalances.getBalance(address))
+    )
+    router.post(
+      '/assign',
+      _(({body: {address}}) => wallets.assignWallet(address))
+    )
+    router.post(
+      '/create',
+      _(({body: {address}}) => wallets.createWallet(address))
+    )
+    router.post(
+      '/createWallets',
+      _(({body: {addresses}}) => wallets.createWallets(addresses))
+    )
+    router.get(
+      '/wallets/blockchainBalance',
+      _(({query: {address}}) => wallets.getWalletBalanceInBlockchain(address))
+    )
   },
 }
