b368b55232e18ece224891dd20bcc5453f2e7985
Elad Heller <eladheller@gmail.com>
2018-03-19 23:39:10 +0200

    improve demo service

--- demo-service/src/queues/listeners/queue-demo.js
+++ demo-service/src/queues/listeners/queue-demo.js
@@ -1,5 +1,5 @@
 const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
 
-module.exports = async (message) => {
-  logger.info({message}, 'SPAM')
+module.exports = async ({body, headers}) => {
+  logger.info({body, headers}, 'SPAM')
 }
