7eb4555ba8902698c7b5017dde24ee865e51eb15
Yair Oval <yairov@gmail.com>
2018-03-20 18:25:12 +0200

    minor refactoring

--- wallets-issuance-monitor/src/queues/listeners/createWallet.js
+++ wallets-issuance-monitor/src/queues/listeners/createWallet.js
@@ -7,16 +7,16 @@ module.exports = async ({body: request}) => {
   }
 
   const {wallets, tokensBalances, tokensTransfers} = services
-
-  // TODO: is this the right property?
   const wallet = request.transcations[0].to
   await wallets.createWallet(wallet)
   const tokenBalances = (await wallets.getWalletBalanceInBlockchain(wallet))
     .filter(({balance}) => balance)
+
   await Promise.all(tokenBalances.map(async ({token, balance}) => {
-    await tokensBalances.updateBalance(token, wallet, balance)
-    // TODO: get all transactions?
-    const transcation = {amount: balance, to: wallet}
-    await tokensTransfers.sendTransactionsToBackend(token, wallet, [transcation], balance, new Date())
+    if (balance > 0) {
+      await tokensBalances.updateBalance(token, wallet, balance)
+      const transcation = {amount: balance, to: wallet}
+      await tokensTransfers.sendTransactionsToBackend(token, wallet, [transcation], balance, new Date())
+    }
   }))
 }
