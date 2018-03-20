1958acfd92dbd1f8e0e87ee3ab5de8019bd092f5
Elad Heller <eladheller@gmail.com>
2018-03-20 10:54:48 +0200

    create wallet queue

--- wallets-issuance-monitor/src/index.js
+++ wallets-issuance-monitor/src/index.js
@@ -1,22 +1,9 @@
-require('app-module-path').addPath(__dirname) // eslint-disable-line import/no-unresolved
-const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
-const {createService} = require('stox-common')
-const {contractsDir, models, initContext} = require('stox-bc-wallet-common')
+require('app-module-path').addPath(__dirname)
+const {createServiceFromFileStructure} = require('stox-common')
 const config = require('config')
-const requireAll = require('require-all')
-const path = require('path')
+const {loggers: {logger}} = require('@welldone-software/node-toolbelt')
+const {initContext} = require('stox-bc-wallet-common')
 
-const jobs = requireAll(path.resolve(__dirname, 'jobs'))
-
-const {databaseUrl, web3Url, mqConnectionUrl} = config
-
-const builderFunc = (builder) => {
-  builder.db(databaseUrl, models)
-  builder.blockchain(web3Url, contractsDir)
-  builder.addQueues(mqConnectionUrl)
-  builder.addJobs(jobs)
-}
-
-createService('wallets-sync', builderFunc)
+createServiceFromFileStructure(__dirname, {name: 'wallets-issuance-monitor'})
   .then(context => initContext({...context, config}))
   .catch(e => logger.error(e))
--- wallets-issuance-monitor/src/queues/listeners/createWallet.js
+++ wallets-issuance-monitor/src/queues/listeners/createWallet.js
@@ -1,13 +1,50 @@
-const {services, context} = require('stox-bc-wallet-common')
+const {services, context: {mq}} = require('stox-bc-wallet-common')
+const {network} = require('config')
+const {exceptions: {UnexpectedError}} = require('@welldone-software/node-toolbelt')
+
+const sendTransactionsToBackend = async (asset, address, transactions, balance, happenedAt) => {
+    const message = {
+      network,
+      address,
+      asset,
+      balance,
+      happenedAt,
+      transactions,
+    }
+  
+    try {
+      mq.publish('blockchain-token-transfers', message)
+  
+      const rest = omit(message, 'transactions')
+      context.logger.info(
+        {
+          ...rest,
+          transactions: transactions.length,
+          hash: transactions.map(t => t.transactionHash),
+        },
+        'SEND_TRANSACTIONS'
+      )
+    } catch (e) {
+      logError(e)
+    }
+  }
 
 module.exports = async ({body: request}) => {
-    if (!request.error) {
-         // TODO: is this the right property?
-        await services.wallets.createWallet(request.transcations[0].to)
-        // TODO: get wallet balance and transactions
-        const balances = []
-        if (balances) {
-            context.mq.publish('blockchain-token-transfers', balances)
-        }
+    if (request.error) {
+        throw new UnexpectedError('Received request with error')
     }
+        
+    const {tokens, wallets, tokensBalances} = services
+
+    // TODO: is this the right property?
+    const wallet = equest.transcations[0].to
+    await services.wallets.createWallet(wallet)
+    const tokenBalances = (await wallets.getWalletBalanceInBlockchain(wallet))
+        .filter(({balance}) => balance)
+    await Promise.all(tokenBalances.map(({token, balance}) => {
+        await tokensBalances.updateBalance(token, wallet, balance)
+        // TODO: get all transactions?
+        const transcation = {amount: balance, status: 'confirmed', type: 'deposit'}
+        await sendTransactionsToBackend(token, wallet, [transcation], balance, new Date())
+    }))
 }
