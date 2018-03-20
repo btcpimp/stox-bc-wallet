c4d8ad59d3ace4c2cadb16612c26e6c32977cd07
Elad Heller <eladheller@gmail.com>
2018-03-20 12:30:16 +0200

    dry

--- wallets-issuance-monitor/src/queues/listeners/createWallet.js
+++ wallets-issuance-monitor/src/queues/listeners/createWallet.js
@@ -1,50 +1,22 @@
-const {services, context: {mq}} = require('stox-bc-wallet-common')
-const {network} = require('config')
+const {services} = require('stox-bc-wallet-common')
 const {exceptions: {UnexpectedError}} = require('@welldone-software/node-toolbelt')
 
-const sendTransactionsToBackend = async (asset, address, transactions, balance, happenedAt) => {
-    const message = {
-      network,
-      address,
-      asset,
-      balance,
-      happenedAt,
-      transactions,
-    }
-  
-    try {
-      mq.publish('blockchain-token-transfers', message)
-  
-      const rest = omit(message, 'transactions')
-      context.logger.info(
-        {
-          ...rest,
-          transactions: transactions.length,
-          hash: transactions.map(t => t.transactionHash),
-        },
-        'SEND_TRANSACTIONS'
-      )
-    } catch (e) {
-      logError(e)
-    }
+module.exports = async ({body: request}) => {
+  if (request.error) {
+    throw new UnexpectedError('Received request with error')
   }
 
-module.exports = async ({body: request}) => {
-    if (request.error) {
-        throw new UnexpectedError('Received request with error')
-    }
-        
-    const {tokens, wallets, tokensBalances} = services
+  const {wallets, tokensBalances, tokensTransfers} = services
 
-    // TODO: is this the right property?
-    const wallet = equest.transcations[0].to
-    await services.wallets.createWallet(wallet)
-    const tokenBalances = (await wallets.getWalletBalanceInBlockchain(wallet))
-        .filter(({balance}) => balance)
-    await Promise.all(tokenBalances.map(({token, balance}) => {
-        await tokensBalances.updateBalance(token, wallet, balance)
-        // TODO: get all transactions?
-        const transcation = {amount: balance, status: 'confirmed', type: 'deposit'}
-        await sendTransactionsToBackend(token, wallet, [transcation], balance, new Date())
-    }))
+  // TODO: is this the right property?
+  const wallet = request.transcations[0].to
+  await wallets.createWallet(wallet)
+  const tokenBalances = (await wallets.getWalletBalanceInBlockchain(wallet))
+    .filter(({balance}) => balance)
+  await Promise.all(tokenBalances.map(async ({token, balance}) => {
+    await tokensBalances.updateBalance(token, wallet, balance)
+    // TODO: get all transactions?
+    const transcation = {amount: balance, to: wallet}
+    await tokensTransfers.sendTransactionsToBackend(token, wallet, [transcation], balance, new Date())
+  }))
 }
