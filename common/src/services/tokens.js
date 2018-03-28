fa8f187b6987b4dac883fed68522267805029518
Elad Heller <eladheller@gmail.com>
2018-03-28 10:07:20 +0300

    linter

--- common/src/services/tokens.js
+++ common/src/services/tokens.js
@@ -1,7 +1,8 @@
 const {db, config} = require('../context')
 const {exceptions: {NotFoundError}} = require('@welldone-software/node-toolbelt')
+
 const getTokens = () => db.tokens.findAll({where: {network: config.network}})
-const getTokenAddress = async (tokenId) =>  {
+const getTokenAddress = async (tokenId) => {
   const token = await db.tokens.findOne({where: {network: config.network, name: tokenId}})
   if (!token) {
     throw new NotFoundError('Token not found', {token})
