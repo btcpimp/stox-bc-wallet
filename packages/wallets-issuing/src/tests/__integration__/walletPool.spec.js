659a89f8b4a44c4407765d010be1918e862c6f26
Ariel Cohen | Stox <ariel@ip-192-168-100-134.eu-west-1.compute.internal>
2018-05-07 12:18:18 +0300

    return tests

--- packages/wallets-issuing/src/tests/__integration__/walletPool.spec.js
+++ packages/wallets-issuing/src/tests/__integration__/walletPool.spec.js
@@ -79,56 +79,54 @@ describe('wallet pool tests', () => {
     'should add pending request to db to meet ' +
       'the required thresh hold and send request on queue to replenish wallets ',
     async () => {
-      expect(true).toBe(true)
-
-      // const pendingCount = Math.round(walletThreshHoldNumber / 2)
-      // await createTest({
-      //   initialNumberOfPendingRequests: pendingCount,
-      //   expectedNumberOfCallsToQueue: walletThreshHoldNumber - pendingCount,
-      //   expectedNumberOfPendingRequests: walletThreshHoldNumber,
-      // })
+      const pendingCount = Math.round(walletThreshHoldNumber / 2)
+      await createTest({
+        initialNumberOfPendingRequests: pendingCount,
+        expectedNumberOfCallsToQueue: walletThreshHoldNumber - pendingCount,
+        expectedNumberOfPendingRequests: walletThreshHoldNumber,
+      })
     }
   )
 
-  // it(
-  //   'should not send any wallet request and not' +
-  //     ' add to pending request when enough wallets are assigned and no pending requests',
-  //   () =>
-  //     createTest({
-  //       wallets: createWallets(walletThreshHoldNumber + 1),
-  //       initialNumberOfPendingRequests: 0,
-  //       expectedNumberOfCallsToQueue: 0,
-  //       expectedNumberOfPendingRequests: 0,
-  //     })
-  // )
+  it(
+    'should not send any wallet request and not' +
+      ' add to pending request when enough wallets are assigned and no pending requests',
+    () =>
+      createTest({
+        wallets: createWallets(walletThreshHoldNumber + 1),
+        initialNumberOfPendingRequests: 0,
+        expectedNumberOfCallsToQueue: 0,
+        expectedNumberOfPendingRequests: 0,
+      })
+  )
 
-  // it(
-  //   'should send wallet requests and add to pending request when not enough wallets are unassigned' +
-  //     ' and not enough pending request combine meets the thresh hold',
-  //   async () => {
-  //     const numberOfUnAlignedWallets = Math.round(walletThreshHoldNumber / 4)
-  //     const numberOfPendingRequests = Math.round(walletThreshHoldNumber / 4)
-  //     const expectedNumberOfCallsToQueue = walletThreshHoldNumber - numberOfPendingRequests - numberOfUnAlignedWallets
-  //     const expectedNumberOfPendingRequests = walletThreshHoldNumber - numberOfUnAlignedWallets
+  it(
+    'should send wallet requests and add to pending request when not enough wallets are unassigned' +
+      ' and not enough pending request combine meets the thresh hold',
+    async () => {
+      const numberOfUnAlignedWallets = Math.round(walletThreshHoldNumber / 4)
+      const numberOfPendingRequests = Math.round(walletThreshHoldNumber / 4)
+      const expectedNumberOfCallsToQueue = walletThreshHoldNumber - numberOfPendingRequests - numberOfUnAlignedWallets
+      const expectedNumberOfPendingRequests = walletThreshHoldNumber - numberOfUnAlignedWallets
 
-  //     await createTest({
-  //       wallets: createWallets(numberOfUnAlignedWallets),
-  //       initialNumberOfPendingRequests: numberOfPendingRequests,
-  //       expectedNumberOfCallsToQueue,
-  //       expectedNumberOfPendingRequests,
-  //     })
-  //   }
-  // )
+      await createTest({
+        wallets: createWallets(numberOfUnAlignedWallets),
+        initialNumberOfPendingRequests: numberOfPendingRequests,
+        expectedNumberOfCallsToQueue,
+        expectedNumberOfPendingRequests,
+      })
+    }
+  )
 
-  // it('should not add send requests for assigned wallets ', async () => {
-  //   const numberOfAssingedWallets = Math.round(walletThreshHoldNumber / 4)
-  //   const initialNumberOfPendingRequests = Math.round(walletThreshHoldNumber / 4)
-  //   const expectedNumberOfCallsToQueue = walletThreshHoldNumber - initialNumberOfPendingRequests
-  //   await createTest({
-  //     wallets: createWallets(numberOfAssingedWallets, new Date()),
-  //     initialNumberOfPendingRequests,
-  //     expectedNumberOfCallsToQueue,
-  //     expectedNumberOfPendingRequests: walletThreshHoldNumber,
-  //   })
-  // })
+  it('should not add send requests for assigned wallets ', async () => {
+    const numberOfAssingedWallets = Math.round(walletThreshHoldNumber / 4)
+    const initialNumberOfPendingRequests = Math.round(walletThreshHoldNumber / 4)
+    const expectedNumberOfCallsToQueue = walletThreshHoldNumber - initialNumberOfPendingRequests
+    await createTest({
+      wallets: createWallets(numberOfAssingedWallets, new Date()),
+      initialNumberOfPendingRequests,
+      expectedNumberOfCallsToQueue,
+      expectedNumberOfPendingRequests: walletThreshHoldNumber,
+    })
+  })
 })
