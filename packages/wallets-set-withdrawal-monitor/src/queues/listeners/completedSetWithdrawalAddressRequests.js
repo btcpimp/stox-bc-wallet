f00bed06286276be09d32eba27ab191eef8bc056
Danny <danny@stox.com>
2018-05-28 11:11:29 +0300

    * Added logging * Removed internal string from .env.example

--- packages/wallets-set-withdrawal-monitor/src/queues/listeners/completedSetWithdrawalAddressRequests.js
+++ packages/wallets-set-withdrawal-monitor/src/queues/listeners/completedSetWithdrawalAddressRequests.js
@@ -1,4 +1,5 @@
 const {
+  context,
   services: {wallets},
 } = require('stox-bc-wallet-common')
 
@@ -6,6 +7,7 @@ module.exports = async ({body: completedRequest}) => {
   if (!completedRequest.error) {
     const wallet = await wallets.getWalletByAddress(completedRequest.data.walletAddress)
     await wallets.updateWallet(wallet, {setWithdrawAddressAt: Date.now()})
+  } else {
+    context.logger.error(completedRequest, 'ERROR_SET_WITHDRAWAL_ADDRESS')
   }
-
 }
