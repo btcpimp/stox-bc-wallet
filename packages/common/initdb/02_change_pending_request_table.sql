caa7962f4a053cbdfe115ff92744f09a25d50bb6
ariel <ariel@stox.com>
2018-10-04 13:25:45 +0300

    fix: taskRoleArn for dev

--- packages/common/initdb/02_change_pending_request_table.sql
+++ packages/common/initdb/02_change_pending_request_table.sql
@@ -3,5 +3,6 @@ drop table "pendingRequests";
 CREATE TABLE "pendingRequests"
 (
    "requestId" UUID NOT NULL PRIMARY KEY,
-   type CHARACTER VARYING(30)
+   type CHARACTER VARYING(30),
+   "createdAt" timestamp with time zone default CURRENT_DATE NOT NULL
 );
