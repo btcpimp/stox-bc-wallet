b4ba80428dde5a782935d188113bca5855932534
ariel <ariel@stox.com>
2018-10-04 16:58:42 +0300

    fix: finishPendingRequest with error

--- packages/wallets-set-withdrawal-monitor/src/queues/listeners/completedSetWithdrawalAddressRequests.js
+++ packages/wallets-set-withdrawal-monitor/src/queues/listeners/completedSetWithdrawalAddressRequests.js
@@ -6,10 +6,10 @@ const {
 module.exports = async ({body: completedRequest}) => {
   const dbTransaction = await context.db.sequelize.transaction()
   try {
+    await pendingRequests.finishPendingRequest(completedRequest.id, dbTransaction)
     if (completedRequest.error) {
       throw completedRequest.error
     }
-    await pendingRequests.finishPendingRequest(completedRequest.id, dbTransaction)
     const wallet = await wallets.getWalletByAddress(completedRequest.data.walletAddress)
     await wallet.updateAttributes({setWithdrawAddressAt: new Date()}, dbTransaction)
     context.mq.publish('bc-assigned-wallets', {
@@ -17,7 +17,12 @@ module.exports = async ({body: completedRequest}) => {
     })
     context.logger.info(completedRequest.id, 'SET_WITHDRAWAL_ADDRESS_SUCCESSFULLY')
   } catch (e) {
-    context.logger.error({...e, requestId: completedRequest.id}, 'ERROR_SET_WITHDRAWAL_ADDRESS')
+    context.logger.error(
+      {
+        transactions: completedRequest.transactions.map(transaction => transaction.transactionHash),
+        requestId: completedRequest.id},
+      'ERROR_SET_WITHDRAWAL_ADDRESS'
+    )
   }
   await dbTransaction.commit()
 }
